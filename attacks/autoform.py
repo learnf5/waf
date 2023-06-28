2c9585fd3ee1d8911085dc2c9c659ee8c92175d9
eriknovak <erik_novak@yahoo.com>
2023-06-28 09:33:53 -0700

    Update autoform.py

--- attacks/autoform.py
+++ attacks/autoform.py
@@ -1,10 +1,10 @@
 import requests
 import json
-from faker import faker
+from faker import Faker
 
 config = json.loads(open('./config.json').read())
 
-faker = faker()
+faker = Faker()
 
 print('Starting...')
 
